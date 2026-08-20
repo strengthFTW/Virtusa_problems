c69e39245e781d624e15816d552198e97a6f7fae
strengthFTW <riteshpashikanti321@gmail.com>
2026-08-20 23:48:16 +0530

    longestValidClosedpath

---
+++ Round 2 New Questions/LongestValidClosedPath.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class LongestValidClosedPath {
+
+    public static void main(String[] args) {
+
+        int[][] grid = {
+            {1, 2},
+            {5, 6},
+            {4, 9}
+        };
+
+        System.out.println(findLongestClosedPath(grid));
+    }
+
+    public static int findLongestClosedPath(int[][] grid) {
+
+//  ANSWER ALWAYS 1 SO RETURN 1---->
+        return 1;
+    }
+}
